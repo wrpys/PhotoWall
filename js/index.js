35cc85066c0f38ce23b426953e834171561ba882
wrpys <496732709@qq.com>
2017-01-08 16:53:34 +0800

    图片及框架

--- js/index.js
+++ js/index.js
@@ -11,12 +11,34 @@ var PhotoWall={
 		PhotoWall.imagesDatas = (function(imageDatasArr){
 			for(var i=0, len=imageDatasArr.length; i<len; i++) {
 				var singleImageData = imageDatasArr[i];
-				singleImageData.imageURL = "../iamges/" + singleImageData.fileName;
+				singleImageData.imageURL = "images/" + singleImageData.fileName;
 				imageDatasArr[i] = singleImageData;
 			}
 			return imageDatasArr;
 		})(imagesDatas);
 
 		console.log(PhotoWall.imagesDatas);
+
+		var controllerUnits = [],
+			imgFigures = [];
+
+		for(var i=0, len=PhotoWall.imagesDatas.length; i<len; i++) {
+			var data = PhotoWall.imagesDatas[i];
+			imgFigures.push(PhotoWall.generatorFigure(data));
+		}
+
+		var imgSec = $(".img-sec");
+		imgSec.append(imgFigures.join(""));
+
+	},
+	generatorFigure: function(data) {
+		var _html = [];
+		_html.push('<figure class="img-figure">');
+		_html.push('	<img src="' + data.imageURL + '" alt="' + data.title + '"/>');
+		_html.push('	<figcaption>');
+		_html.push('		<h2 class="img-title">' + data.title + '</h2>');
+		_html.push('	</figcaption>');
+		_html.push('</figure>');
+		return _html.join("");
 	}
 }
